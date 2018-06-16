f80aaf3f8e27f71bb6452431f5b6fe6405968445
Nirmal <jitnirmal@gmail.com>
2018-06-16 21:14:04 +0800

    changed build file

--- scripts/vsbuild.bat
+++ scripts/vsbuild.bat
@@ -1,8 +1,8 @@
 ECHO OFF
 ECHO starting program
-
-cd ..
-cmake -H. -Bvsbuild
+set BUILD="C:\Users\jitni\Documents\GitHub\"
+set REPO="C:\Users\jitni\Documents\GitHub\CodeBase Git"
+cd %BUILD%
+cmake %REPO%
 cmake --build vsbuild --
-
 PAUSE
